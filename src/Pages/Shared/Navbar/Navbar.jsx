30052fd2b16ffc8dc1181a2bd798f1adcadc0f4d
Md Saidur Rahman <saidurrahmanbanguri1997@gmail.com.com>
2023-06-07 22:43:47 +0600

    navbar added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
    
   </>
   return (
-    <div className="navbar fixed bg-opacity-20 bg-black text-white z-10 max-w-7xl">
+    <div className="navbar bg-slate-500 text-white max-w-7xl">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
