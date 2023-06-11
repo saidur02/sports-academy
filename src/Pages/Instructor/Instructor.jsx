eea7ffc2c136665a62086b451854dc68bd2b7746
Md Saidur Rahman <saidurrahmanbanguri1997@gmail.com.com>
2023-06-12 02:55:57 +0600

    admin route added

--- src/Pages/Instructor/Instructor.jsx
+++ src/Pages/Instructor/Instructor.jsx
@@ -45,20 +45,3 @@ const Instructor = () => {
 };
 
 export default Instructor;
-{/* <tbody id={instructor._id}>
-                           
-                           <tr>
-                               <td>
-                                   <div className="flex items-center space-x-3">
-                                       <div className="avatar">
-                                           <div className="mask mask-squircle w-12 h-12">
-                                               <img src='' alt="Avatar Tailwind CSS Component" />
-                                           </div>
-                                       </div>
-                                   </div>
-                               </td>
-                               <td>{instructor.name} </td>
-                               <td>{Instructor.email}</td>
-                           </tr>
-
-                       </tbody> */}
